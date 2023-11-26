ebc71daa24e777d4478ba8fadfb3db7f0c89b515
Federico Piñan <85468675+federicopinan@users.noreply.github.com>
2023-11-26 02:19:43 -0300

    Cambios en el código
    Cambiamos, el login, lista de productos y asignación

--- servicios/categorias-servicios.js
+++ servicios/categorias-servicios.js
@@ -1,54 +1,48 @@
-const url = "https://65418746f0b8287df1fe755a.mockapi.io/api/TpiLab3/categorias";
-
+const url =
+    "https://65418746f0b8287df1fe755a.mockapi.io/api/TpiLab3/categorias";
 
 async function listar(id) {
     let cadUrl;
-    if(isNaN(id))
-      cadUrl= url;
-    else 
-      cadUrl = url + "/" + id;  
-    return await fetch(cadUrl)
-        .then(respuesta => respuesta.json());
+    if (isNaN(id)) cadUrl = url;
+    else cadUrl = url + "/" + id;
+    return await fetch(cadUrl).then(respuesta => respuesta.json());
 }
 
 async function crear(descripcion) {
-
     return await fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            descripcion: descripcion
-        })
-    })
+            descripcion: descripcion,
+        }),
+    });
 }
 
 async function editar(id, descripcion) {
-
     let urlPut = url + "/" + id;
     return await fetch(urlPut, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            descripcion: descripcion
-        })
-    })
+            descripcion: descripcion,
+        }),
+    });
 }
 
-async function borrar(id){
-  
+async function borrar(id) {
     let urlPut = url + "/" + id;
     return await fetch(urlPut, {
-            method: 'DELETE'
-       })
+        method: "DELETE",
+    });
 }
 
 export const categoriasServices = {
     listar,
     crear,
     editar,
-    borrar
-}
+    borrar,
+};
--- servicios/usuarios-servicios.js
+++ servicios/usuarios-servicios.js
@@ -3,71 +3,88 @@ const url = "https://65418746f0b8287df1fe755a.mockapi.io/api/TpiLab3/usuarios";
 //API-REST USUARIOS//
 
 async function listar(id) {
-    let cadUrl;
-    if(isNaN(id))
-      cadUrl= url;
-    else 
-      cadUrl = url + "/" + id;  
-    return await fetch(cadUrl)
-        .then(respuesta => respuesta.json());
+  let cadUrl;
+  if (isNaN(id)) cadUrl = url;
+  else cadUrl = url + "/" + id;
+  return await fetch(cadUrl).then(respuesta => respuesta.json());
 }
 
-async function crear(apellido, nombre, correo, password, avatar, pais, ciudad, direccion, telefono, role="admin") {
-
-    return await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            apellido: apellido,
-            nombre: nombre,
-            correo: correo,
-            password: password,
-            avatar: avatar,
-            pais: pais,
-            ciudad: ciudad,
-            direccion: direccion,
-            telefono:  telefono,
-            role: role
-        })
-    })
+async function crear(
+  apellido,
+  nombre,
+  correo,
+  password,
+  avatar,
+  pais,
+  ciudad,
+  direccion,
+  telefono,
+  role = "admin"
+) {
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      apellido: apellido,
+      nombre: nombre,
+      correo: correo,
+      password: password,
+      avatar: avatar,
+      pais: pais,
+      ciudad: ciudad,
+      direccion: direccion,
+      telefono: telefono,
+      role: role,
+    }),
+  });
 }
 
-async function editar(id, apellido, nombre, correo, password, avatar, pais, ciudad, direccion, telefono, role="admin") {
-
-    let urlPut = url + "/" + id;
-    return await fetch(urlPut, {
-        method: 'PUT',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            apellido: apellido,
-            nombre: nombre,
-            correo: correo,
-            password: password,
-            avatar: avatar,
-            pais: pais,
-            ciudad: ciudad,
-            direccion: direccion,
-            telefono:  telefono,
-            role: role
-        })
-    })
+async function editar(
+  id,
+  apellido,
+  nombre,
+  correo,
+  password,
+  avatar,
+  pais,
+  ciudad,
+  direccion,
+  telefono,
+  role = "admin"
+) {
+  let urlPut = url + "/" + id;
+  return await fetch(urlPut, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      apellido: apellido,
+      nombre: nombre,
+      correo: correo,
+      password: password,
+      avatar: avatar,
+      pais: pais,
+      ciudad: ciudad,
+      direccion: direccion,
+      telefono: telefono,
+      role: role,
+    }),
+  });
 }
 
-async function borrar(id){
-  
-    let urlPut = url + "/" + id;
-    return await fetch(urlPut, {
-            method: 'DELETE'
-       })
+async function borrar(id) {
+  let urlPut = url + "/" + id;
+  return await fetch(urlPut, {
+    method: "DELETE",
+  });
 }
 
 export const usuariosServices = {
-    listar,
-    crear,
-    editar,
-    borrar
-}
+  listar,
+  crear,
+  editar,
+  borrar,
+};
